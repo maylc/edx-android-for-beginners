70bdaae5d5383d3bcb46c39a1e8f1f36b0667563
Mayara Cabral <mayaralcabral@gmail.com>
2017-09-08 10:40:08 -0400

    TipCalculator
    - Finished lesson "Making our App Functional (Part 1)"
    -- included ButterKnife
    -- started app logic

--- TipCalculator/app/src/main/java/com/maylc/tipcalculator/MainActivity.java
+++ TipCalculator/app/src/main/java/com/maylc/tipcalculator/MainActivity.java
@@ -2,12 +2,65 @@ package com.maylc.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @BindView(R.id.etBillAmount)
+    EditText etBillAmount;
+    @BindView(R.id.tvTipPercent)
+    TextView tvTipPercent;
+    @BindView(R.id.tvTipAmount)
+    TextView tvTipAmount;
+    @BindView(R.id.tvBillTotalAmount)
+    TextView tvBillTotalAmount;
+
+    float percentage = 0;
+    float tipTotal = 0;
+    float finalBillAmount = 0;
+
+    float REGULAR_TIP_PERCENTAGE  = 10;
+    float DEFAULT_TIP_PERCENTAGE  = 15;
+    float EXCELENT_TIP_PERCENTAGE = 20;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setTipValues();
+    }
+
+    private void setTipValues()
+    {
+        tvTipPercent.setText(getString(R.string.main_msg_tippercent, percentage));
+        tvTipAmount.setText(getString(R.string.main_msg_tiptotal, tipTotal));
+        tvBillTotalAmount.setText(getString(R.string.main_msg_billtotalresult, finalBillAmount));
+    }
+
+    @OnClick({R.id.ibRegularService, R.id.ibGoodService, R.id.ibExcellentService})
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.ibRegularService:
+                percentage = REGULAR_TIP_PERCENTAGE;
+                break;
+            case R.id.ibGoodService:
+                percentage = DEFAULT_TIP_PERCENTAGE;
+                break;
+            case R.id.ibExcellentService:
+                percentage = EXCELENT_TIP_PERCENTAGE;
+                break;
+        }
     }
 }
