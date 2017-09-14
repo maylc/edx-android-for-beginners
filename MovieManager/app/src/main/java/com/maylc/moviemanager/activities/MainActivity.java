2508c363afc12dafd592c4386a0e852c0563f694
Mayara Cabral <mayaralcabral@gmail.com>
2017-09-14 15:57:12 -0400

    Movie Manager App
    - Add Now Playing Fragment and Upcoming Fragment

--- MovieManager/app/src/main/java/com/maylc/moviemanager/activities/MainActivity.java
+++ MovieManager/app/src/main/java/com/maylc/moviemanager/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.maylc.moviemanager;
+package com.maylc.moviemanager.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,11 +15,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.maylc.moviemanager.R;
+import com.maylc.moviemanager.fragments.NowPlayingFragment;
+import com.maylc.moviemanager.fragments.UpcomingFragment;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -40,6 +47,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        showFragment(NowPlayingFragment.class);
     }
 
     @Override
@@ -80,16 +89,45 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_now_playing) {
-            // Handle the camera action
-        } else if (id == R.id.nav_upcoming) {
+        Class fragment = null;
 
-        } else if (id == R.id.nav_logout) {
+        if (id == R.id.nav_now_playing)
+        {
+            fragment = NowPlayingFragment.class;
+            showFragment(fragment);
+        }
+        else if (id == R.id.nav_upcoming)
+        {
+            fragment = UpcomingFragment.class;
+            showFragment(fragment);
+        }
+        else if (id == R.id.nav_logout) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void showFragment(Class fragmentClass)
+    {
+        Fragment fragment = null;
+
+        try
+        {
+             fragment = (Fragment)fragmentClass.newInstance();
+        }
+        catch (InstantiationException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.flContent, fragment).commit();
     }
 }
